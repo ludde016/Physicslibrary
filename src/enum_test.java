c8cd2e5139e0dba92b5b94ebe7871659e9d2909c
ludde016 <ludde.swala@gmail.com>
2018-11-06 22:41:32 +0100

    Commit #26
    Det är många ändringar för att jag tryckte Ctrl+shift+f. Förrutom det så har jag lagt till 4 metoder som gör följande: beräknar mängden hästkrafter från watt, hur mycket joule det tar för en vätska att gå från flytande till ånga, hur mycket joule det tar för ett fast ämne att övergå från fast till flytande OCH hur man får fram densitet från massa och volym.

--- src/enum_test.java
+++ src/enum_test.java
@@ -7,11 +7,11 @@ public class enum_test {
 	static double ATM = 101325;
 	static double c = 299792458;
 	static double G = 9.82;
-	
+
 	public static void main(String[] args) {
-		Scanner input = new Scanner (System.in); 
-		double intag = input.nextDouble(); 
-		
+		Scanner input = new Scanner(System.in);
+		double intag = input.nextDouble();
+
 		System.out.println(fahrenheitToCelsius(intag));
 		System.out.println(kelvinToCelsius(intag));
 		System.out.println(pressureUnderWater(10));
@@ -34,166 +34,194 @@ public class enum_test {
 	}
 
 	public static double fahrenheitToCelsius(double fahrenheit) {
-		
-		double celsius = (fahrenheit - 32)/1.8;
+
+		double celsius = (fahrenheit - 32) / 1.8;
 
 		return celsius;
 	}
 
 	public static double kelvinToCelsius(double kelvin) {
-		
-		double Celsius = kelvin - 273.15; 
-		
-		return Celsius; 
+
+		double Celsius = kelvin - 273.15;
+
+		return Celsius;
 	}
-	
+
 	public static double fluidpressure(Fluidtable fluid, double deep) {
-		double pressure = fluid.density * G * deep; // Ger inget värde i konsolen. 
-		
-		return pressure; 
+		double pressure = fluid.density * G * deep; // Ger inget värde i konsolen.
+
+		return pressure;
 	}
-	
+
 	public static double pressureUnderWater(double deep) {
-		return fluidpressure(Fluidtable.WATER, deep); // En metod som endast använder förra metoden fast sätter in vätskan "WATER"
+		return fluidpressure(Fluidtable.WATER, deep); // En metod som endast använder förra metoden fast sätter in
+														// vätskan "WATER"
 	}
 
 	public static double kineticEnergy(double mass, double velocity) {
-		double energy = (mass * velocity * velocity)/2;
-		
-		return energy; 
+		double energy = (mass * velocity * velocity) / 2;
+
+		return energy;
 	}
-	
+
 	public static double potentialEnergy(double mass, double height) {
 		double energy = mass * g_swe * height;
-		
-		return energy; 
+
+		return energy;
 	}
-	
+
 	public static double fallSpeed(double height) {
 		double speed = Math.sqrt(2 * G * height);
-		
+
 		return speed;
 	}
-	
+
 	public static double delta(double first, double last) {
 		double delta = last - first;
-		
+
 		return delta;
 	}
-	
+
 	public static double volumeToMass(Fluidtable fluid, double volume) {
 		double mass = fluid.density * volume;
-		
-		return mass; 
+
+		return mass;
 	}
-	
+
 	public static double volumeToMass(Gastable gas, double volume) {
 		double mass = gas.density * volume;
-		
+
 		return mass;
 	}
-	
+
 	public static double volumeToMass(Solidtable solid, double volume) {
 		double mass = solid.density * volume;
-		
+
 		return mass;
 	}
-	
+
 	public static double svtVelocity(double distance, double time) {
-		double deltaV = distance/time; 
-		
-		return deltaV; 
+		double deltaV = distance / time;
+
+		return deltaV;
 	}
-	
+
 	public static double svtDistance(double velocity, double time) {
-		double distance = velocity * time; 
-		
-		return distance; 
+		double distance = velocity * time;
+
+		return distance;
 	}
-	
+
 	public static double svtTime(double distance, double velocity) {
 		double time = distance / velocity;
-		
-		return time; 
+
+		return time;
 	}
-	
-	public static double  work(double force, double distance) {
+
+	public static double work(double force, double distance) {
 		double work = force * distance;
-		
-		return work; 
+
+		return work;
 	}
-	
+
 	public static double power(double work, double time) {
-		double power = work/time; 
-		
+		double power = work / time;
+
 		return power;
 	}
-	
+
 	public static double heat(Solidtable solid, double mass, double deltaT) {
 		double heat = solid.heatcapacity * mass * deltaT;
-		
+
 		return heat;
 	}
-	
+
 	public static double heat(Fluidtable fluid, double volume, double deltaT) {
-		double heat = fluid.heatcapacity * volume * fluid.density * deltaT;  
-		
-		return heat; 
+		double heat = fluid.heatcapacity * volume * fluid.density * deltaT;
+
+		return heat;
 	}
-	
+
 	public static double heat(Gastable gas, double volume, double deltaT) {
-		double heat = gas.heatcapacity * volume * gas.density * deltaT; 
-		
-		return heat; 
+		double heat = gas.heatcapacity * volume * gas.density * deltaT;
+
+		return heat;
 	}
-	
+
 	public static double velocityToHeight(double velocity) {
-	double height = (velocity/G) * (velocity/2);
-	
-	return height;
+		double height = (velocity / G) * (velocity / 2);
+
+		return height;
 	}
-	
+
 	public static double force(double mass, double acceleration) {
 		double kraft = mass * acceleration;
-		
+
 		return kraft;
-		
+
 	}
-	
+
 	public static double distance(double time, double acceleration) {
-		double distance = (time * time * acceleration)/2;
-		
+		double distance = (time * time * acceleration) / 2;
+
 		return distance;
-		
+
 	}
-	
+
 	public static double friction(double Fn, double My) {
 		double FrictionForce = Fn * My;
-		
+
 		return FrictionForce;
-		
+
 	}
-	
+
 	public static double pressure(double force, double area) {
-		double pressure = force/area;
-		
+		double pressure = force / area;
+
 		return pressure;
-		
+
 	}
-	
+
 	public static double liftForce(Fluidtable fluid, double volume) {
 		double lift = fluid.density * g_swe * volume;
-		
+
 		return lift;
-		
+
 	}
-	
+
 	public static double efficiency(double inputEnergy, double usefulEnergy) {
-		double efficiency = usefulEnergy/inputEnergy;
-		
+		double efficiency = usefulEnergy / inputEnergy;
+
 		return efficiency;
+
+	}
+
+	public static double horsepower(double watt) {
+		double HK = watt / 735.5;
+
+		return HK;
+
+	}
+
+	public static double fluidToSteam(Fluidtable fluid, double mass) {
+		double joule = fluid.steamEntalpy * mass;
+
+		return joule;
+
+	}
+	
+	public static double density(double mass, double volume) {
+		double density = mass/volume;
+		
+		return density;
 		
 	}
 	
-	
+	public static double solidToLiquid(Solidtable solid, double mass) {
+		double joule = solid.meltentalpy * mass;
+		
+		return joule;
+		
+	}
+
 }
